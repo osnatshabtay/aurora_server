2c23707e3912d38b8d13ab12bf361bbcf11ae2a8
osnatshabtay <osnatshabtay98@gmail.com>
2025-01-27 19:31:54 +0200

    chatbot routes start

--- app/main.py
+++ app/main.py
@@ -4,11 +4,12 @@ import os
 
 from app.routes.user_routes import router as user_route
 from app.routes.post_routes import router as post_routes
-
+from app.routes.chatbot_routes import router as chatbot_routes
 app = FastAPI()
 
 app.include_router(user_route, prefix="/users", tags=["Users"])
 app.include_router(post_routes, prefix="/feed", tags=["Post"])
+app.include_router(chatbot_routes, prefix="/chatbot", tags=["Chatbot"])
 
 @app.get("/")
 async def read_root():
